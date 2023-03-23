d83c4e4c62b3225c10a21a2ae39db728efe5fead
Yash sarda <yashsarda2002@gmail.com>
2023-03-24 00:00:34 +0530

    Dance picture added

--- assets/js/dance.js
+++ assets/js/dance.js
@@ -1,58 +1,11 @@
-$(function () {
-  var winh = $(window).height();
-  var winw = $(window).width();
-  $("body").css({
-    height: winh,
-    width: winw,
-  });
-
-  TweenMax.from(".speakerL", 0.3, {
-    scale: 1.1,
-    yoyo: true,
-    repeat: -1,
-    ease: Bounce.easeOut,
-  });
-  TweenMax.from(".speakerR", 0.3, {
-    scale: 1.1,
-    yoyo: true,
-    repeat: -1,
-    ease: Bounce.easeOut,
-  });
-  TweenMax.staggerFrom(
-    ".crowd",
-    0.5,
-    {
-      y: 100,
-      yoyo: true,
-      repeat: -1,
-    },
-    0.1
+(function () {
+  var audio = new Audio(
+    "https://drive.google.com/uc?id=1q_BIdyFQ_gF0zqK-45IpECLJYQQ27zyC&export=download"
   );
-  TweenMax.from(".lightL", 0.5, {
-    x: winw / 2,
-    yoyo: true,
-    repeat: -1,
-    repeatDelay: 2,
-  });
-  TweenMax.from(".lightR", 0.5, {
-    x: -winw / 2,
-    yoyo: true,
-    repeat: -1,
-    repeatDelay: 2,
-  });
-  TweenMax.fromTo(
-    ".psy",
-    5,
-    {
-      backgroundPosition: "0 0",
-      x: -200,
-    },
-    {
-      backgroundPosition: "-4722px 0",
-      repeat: -1,
-      yoyo: true,
-      ease: SteppedEase.config(21),
-      x: 200,
-    }
+  audio.loop = true;
+  audio.play();
+  var controlBtn = document.querySelector("control");
+  document.addEventListener("click", (e) =>
+    audio.paused ? audio.play() : audio.pause()
   );
-});
+})();
